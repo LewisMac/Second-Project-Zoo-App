0128a88fc2c55e65e40df5fb71b5c292944ebaa9
Lewis MacNee <lewis.macnee@hotmail.com>
2016-12-19 11:27:44 +0000

    added method for assigning random sell price (usually less than buy price)

--- app/src/main/java/com/example/user/zooapp/Funds.java
+++ app/src/main/java/com/example/user/zooapp/Funds.java
@@ -66,4 +66,49 @@ public class Funds {
         }
         return price;
     }
+
+    public int assignSellPrice(Animal animal){
+        Random rand = new Random();
+
+        int price = 0;
+
+        switch (animal.getSpecies()){
+            case HIPPOCAMPUS:
+
+            case SELKIE:
+                price = rand.nextInt(20) + 10;
+                break;
+            case SEAHORSE:
+                price = rand.nextInt(10) + 0;
+                break;
+            case GRIFFIN:
+                price = rand.nextInt(40) + 10;
+                break;
+            case HIPPOGRIFF:
+                price = rand.nextInt(30) + 10;
+                break;
+            case YETI:
+                price = rand.nextInt(50) + 20;
+                break;
+            case DRAGON:
+                price = rand.nextInt(50) + 50;
+                break;
+            case UNICORN:
+                price = rand.nextInt(50) + 20;
+                break;
+            case WEREWOLF:
+                price = rand.nextInt(40) + 20;
+                break;
+            case GNOME:
+                price = rand.nextInt(20) + 5;
+                break;
+            case LEPRECHAUN:
+                price = rand.nextInt(20) + 5;
+                break;
+            case CENTAUR:
+                price = rand.nextInt(30) + 10;
+                break;
+        }
+        return price;
+    }
 }
