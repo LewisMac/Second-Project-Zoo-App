c2892a83e6b61ec4fc1525b078da58463f17d1b7
Lewis MacNee <lewis.macnee@hotmail.com>
2016-12-19 11:09:51 +0000

    added a method for assigning a random price for buying animals

--- app/src/main/java/com/example/user/zooapp/Funds.java
+++ app/src/main/java/com/example/user/zooapp/Funds.java
@@ -1,5 +1,7 @@
 package com.example.user.zooapp;
 
+import java.util.Random;
+
 /**
  * Created by user on 16/12/2016.
  */
@@ -17,5 +19,51 @@ public class Funds {
 
     public void addRemoveFunds(int funds) {
         this.funds += funds;
+    }
+
+    public int assignBuyPrice(Animal animal){
+
+        Random rand = new Random();
+
+        int price = 0;
+
+        switch (animal.getSpecies()){
+            case HIPPOCAMPUS:
+
+            case SELKIE:
+                price = rand.nextInt(20) + 20;
+                break;
+            case SEAHORSE:
+                price = rand.nextInt(10) + 5;
+                break;
+            case GRIFFIN:
+                price = rand.nextInt(40) + 20;
+                break;
+            case HIPPOGRIFF:
+                price = rand.nextInt(30) + 20;
+                break;
+            case YETI:
+                price = rand.nextInt(50) + 50;
+                break;
+            case DRAGON:
+                price = rand.nextInt(50) + 100;
+                break;
+            case UNICORN:
+                price = rand.nextInt(50) + 50;
+                break;
+            case WEREWOLF:
+                price = rand.nextInt(40) + 40;
+                break;
+            case GNOME:
+                price = rand.nextInt(20) + 10;
+                break;
+            case LEPRECHAUN:
+                price = rand.nextInt(20) + 20;
+                break;
+            case CENTAUR:
+                price = rand.nextInt(30) + 20;
+                break;
+        }
+        return price;
     }
 }
--- app/src/test/java/com/example/user/zooapp/FundsTest.java
+++ app/src/test/java/com/example/user/zooapp/FundsTest.java
@@ -27,4 +27,21 @@ public class FundsTest {
         funds.addRemoveFunds(300);
         assertEquals(650, funds.getFunds());
     }
+
+    @Test
+    public void testRandomPriceAssigned(){
+        Animal animal = new Animal(SpeciesType.DRAGON, "Fred");
+        Animal animal1 = new Animal(SpeciesType.GNOME, "Fred");
+        Animal animal2 = new Animal(SpeciesType.SEAHORSE, "Fred");
+        Animal animal3 = new Animal(SpeciesType.GNOME, "Fred");
+        Animal animal4 = new Animal(SpeciesType.HIPPOCAMPUS, "Fred");
+
+        System.out.println(funds.assignBuyPrice(animal));
+        System.out.println(funds.assignBuyPrice(animal1));
+        System.out.println(funds.assignBuyPrice(animal2));
+        System.out.println(funds.assignBuyPrice(animal3));
+        System.out.println(funds.assignBuyPrice(animal4));
+
+    }
+
 }
